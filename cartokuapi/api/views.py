df231f501086f1a95dc007c853e6c15f27127de2
Javier Torres <jtorres@carto.com>
2018-09-27 20:20:37 +0200

    Enqueue deploy job on controller

--- cartokuapi/api/views.py
+++ cartokuapi/api/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from api.models import Deploy, App
 from django.core.serializers import serialize
+from api.tasks import deploy
 
-
-#@csrf_exempt 
+#@csrf_exempt
 def push_deploy(request, username, app_name):
     status = request.POST['status']
     applications = App.objects.filter(name=app_name)
@@ -16,10 +16,11 @@ def push_deploy(request, username, app_name):
     deploy = Deploy(status = status, app = application)
     deploy.save()
     deploy_id = deploy.id
-    response = {'deploy_id': deploy_id}
-    return JsonResponse(response)
 
-#@csrf_exempt 
+    deploy.delay(deploy_id)
+    return JsonResponse({'deploy_id': deploy_id})
+
+#@csrf_exempt
 def list_deploys(request, username, app_name):
     try:
         application = App.objects.get(name=app_name)
@@ -27,10 +28,10 @@ def list_deploys(request, username, app_name):
         serialized_deploys = []
         for deploy in deploys:
             serialized_deploys.append({'created_at': deploy.created_at, 'status': deploy.status})
-        return JsonResponse({'deploys' : serialized_deploys})   
+        return JsonResponse({'deploys' : serialized_deploys})
     except App.DoesNotExist:
         return JsonResponse({'deploys': []})
 #
-#@csrf_exempt 
+#@csrf_exempt
 #def show_deploy(username):
 #    pass
