545daf60990a815ff64e3de23fc4b7d4e1b48f28
Javier Torres <jtorres@carto.com>
2018-09-27 13:29:11 +0200

    Add APP model

--- cartokuapi/api/models.py
+++ cartokuapi/api/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class App(models.Model):
+    username = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    oauth_client_id = models.CharField(max_length=64)
+    oauth_client_secret = models.CharField(max_length=64)
+
+
 class Deploy(models.Model):
-    app = models.CharField(max_length=30)
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
     status = models.CharField(max_length=30)
+    log = models.TextField(default='')
