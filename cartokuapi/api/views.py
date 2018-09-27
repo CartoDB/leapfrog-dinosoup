2000e557a7622e90d2c542e8db5ab765accbb1ce
Luis Bosque <luisico@gmail.com>
2018-09-27 17:40:30 +0200

    views

--- cartokuapi/api/views.py
+++ cartokuapi/api/views.py
@@ -6,38 +6,31 @@ from django.core.serializers import serialize
 
 #@csrf_exempt 
 def push_deploy(request, username, app_name):
-	status = request.POST['status']
-	applications = App.objects.filter(name=app_name)
-	if len(applications) == 0:
-		application = App(name=app_name, username = username)
-		application.save()
-	else:
-		application = applications[0]
-	deploy = Deploy(status = status, app = application)
-	deploy.save()
-	deploy_id = deploy.id
-	response = {'deploy_id': deploy_id}
-	return JsonResponse(response)
+    status = request.POST['status']
+    applications = App.objects.filter(name=app_name)
+    if len(applications) == 0:
+        application = App(name=app_name, username = username)
+        application.save()
+    else:
+        application = applications[0]
+    deploy = Deploy(status = status, app = application)
+    deploy.save()
+    deploy_id = deploy.id
+    response = {'deploy_id': deploy_id}
+    return JsonResponse(response)
 
 #@csrf_exempt 
 def list_deploys(request, username, app_name):
-	import pdb; pdb.set_trace()
-	try:
-		application = App.objects.get(name=app_name)
-		#deploys = Deploy.objects.filter(app = application.id)
-		serialized_deploys = serialize('json', Deploy.objects.filter(app = application.id), cls=LazyEncoder)
-		return JsonResponse({'deploys' : serialized_deploys})	
-	except App.DoesNotExist:
-		return JsonResponse({'deploys': []})
+    try:
+        application = App.objects.get(name=app_name)
+        deploys = Deploy.objects.filter(app = application.id)
+        serialized_deploys = []
+        for deploy in deploys:
+            serialized_deploys.append({'created_at': deploy.created_at, 'status': deploy.status})
+        return JsonResponse({'deploys' : serialized_deploys})   
+    except App.DoesNotExist:
+        return JsonResponse({'deploys': []})
 #
 #@csrf_exempt 
 #def show_deploy(username):
 #    pass
-
-from django.core.serializers.json import DjangoJSONEncoder
-
-class LazyEncoder(DjangoJSONEncoder):
-	def default(self, obj):
-		#if isinstance(obj, YourCustomType):
-		#	return str(obj)
-		return super().default(obj)
--- cartokuapi/get_deploys.py
+++ cartokuapi/get_deploys.py
@@ -4,5 +4,5 @@ port = 8000
 url = "http://127.0.0.1:%d/luisico/apps/myapp/deploys" % port
 
 r = requests.get(url)
-print r.status_code
+#print r.status_code
 print r.text
