97b7d13f394f50b3634bf16d7b0b3825f30ad2dc
Luis Bosque <luisico@gmail.com>
2018-09-28 11:40:29 +0200

    Removed ports

--- api_requests.sh
+++ api_requests.sh
@@ -1,5 +1,5 @@
-curl -X POST cartoku:8000/wadus/apps -d name=dummy -d description="dewfre frelfk jreflrekjf lrekj"
-curl cartoku:8000/wadus/apps | python3 -m json.tool
-curl cartoku:8000/wadus/apps/dummy | python3 -m json.tool
-curl cartoku:8000/wadus/apps/dummy/deploys | python3 -m json.tool
-curl cartoku:8000/wadus/apps/dummy/deploys/1 | python3 -m json.tool
+curl -X POST carto.ku/wadus/apps -d name=dummy -d description="dewfre frelfk jreflrekjf lrekj"
+curl carto.ku/wadus/apps | python3 -m json.tool
+curl carto.ku/wadus/apps/dummy | python3 -m json.tool
+curl carto.ku/wadus/apps/dummy/deploys | python3 -m json.tool
+curl carto.ku/wadus/apps/dummy/deploys/1 | python3 -m json.tool
