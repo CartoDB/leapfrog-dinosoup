034094c42529ca9464dbad4f1598a02c4b6100d7
Luis Bosque <luisico@gmail.com>
2018-09-28 07:01:28 +0200

    show app

--- cartokuapi/api/views.py
+++ cartokuapi/api/views.py
@@ -9,7 +9,6 @@ import subprocess
 
 BASE_REPO_PATH = '/srv/git'
 
-#@csrf_exempt
 def push_deploy(request, username, app_name):
     application = App.objects.get(username=username, name=app_name)
     dep = Deploy(status="pending", app=application)
@@ -18,18 +17,19 @@ def push_deploy(request, username, app_name):
     deploy.delay(dep.id)
     return JsonResponse({'deploy_id': dep.id})
 
-#@csrf_exempt
-def list_deploys(request, username, app_name):
+def get_deploys_list (username, app_name):
     try:
         application = App.objects.get(name=app_name)
         deploys = Deploy.objects.filter(app = application.id)
         serialized_deploys = []
         for deploy in deploys:
             serialized_deploys.append({'created_at': deploy.created_at, 'status': deploy.status})
-        return JsonResponse({'deploys' : serialized_deploys})
+        return serialized_deploys
     except App.DoesNotExist:
-        return JsonResponse({'deploys': []})
+        return []
 
+def list_deploys(request, username, app_name):
+    return JsonResponse({'deploys' : get_deploys_list})
 
 def get_deploy(request, username, app_name, deploy_id):
     try:
@@ -45,6 +45,8 @@ def get_deploy(request, username, app_name, deploy_id):
 
 def create_app(request, username):
     appname = request.POST['name']
+    if len(App.objects.filter(name=appname)) > 0:
+        return JsonResponse({'message': "App %s already exists" % appname}, status=400)
     repo_path = BASE_REPO_PATH + "/" + username + "_" + appname + '.git'
     app = App(username=username, name=appname, repo_path=repo_path)
     app.save()
@@ -56,7 +58,19 @@ def create_app(request, username):
         os.fchmod(hook.fileno(), 0o755)
 
     return JsonResponse({'username': app.username, 'name': app.name, 'repo_path': app.repo_path})
-#
-#@csrf_exempt
-#def show_deploy(username):
-#    pass
+
+def show_app(request, username, app_name):
+    print(app_name)
+    try:
+        application = App.objects.get(name=app_name)
+        push_url = "ssh://git@cartoku%s" % application.repo_path
+        app_data = {
+                "name": application.name,
+                "deploy_instructions" : ["git remote add cartoku %s" % push_url, "git push cartoku master"],
+                "username": application.username,
+                "domain": "%s.carto.ku" % application.username,
+                "deploys_list": get_deploys_list(username, app_name)
+                }
+        return JsonResponse(app_data)
+    except App.DoesNotExist:
+        return JsonResponse({})
--- provision.sh
+++ provision.sh
@@ -23,5 +23,11 @@ sed -i 's#ALLOWED_HOSTS = \[\]#ALLOWED_HOSTS = \["*"\]#g' cartokuapi/settings.py
 pip3 install -r requirements.txt
 python3 manage.py migrate
 
-echo -e "\n\n\n\nRun the server now:\nvagrant ssh -c \"sudo python3 /vagrant/cartokuapi/manage.py runserver 0.0.0.0:80\""
-echo -e "\n\n\n\nRun the queues now:\nvagrant ssh -c \"sudo python3 celery -A api worker\""
+# Setup git server
+
+sudo apt-get install git
+adduser git --home /srv/git --gecos "" --disabled-password -q
+sudo -u git mkdir /srv/git/.ssh && sudo -u git cp /vagrant/authorized_keys /srv/git/.ssh
+
+echo -e "\n\n\n\nRun the server now:\nvagrant ssh -c \"sudo -u git python3 /vagrant/cartokuapi/manage.py runserver 0.0.0.0:80\""
+echo -e "\n\n\n\nRun the queues now:\nvagrant ssh -c \"sudo -u git python3 celery -A api worker\""
