02ca2e342a5a44a1c52f246aed12d72dfaa439d7
Luis Bosque <luisico@gmail.com>
2018-09-28 09:49:04 +0200

    Add cors

--- cartokuapi/cartokuapi/settings.py
+++ cartokuapi/cartokuapi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'api'
 ]
 
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'cartokuapi.urls'
 
